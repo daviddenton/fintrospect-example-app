28896a77e8d1c59a382c3241bfe3a7c72875173b
David Denton <denton.david@gmail.com>
2016-03-08 11:23:14 +0000

    dave - fixed compile error. that was embarassing.. :(

--- src/test/scala/env/RunningTestEnvironment.scala
+++ src/test/scala/env/RunningTestEnvironment.scala
@@ -5,7 +5,7 @@ import org.scalatest.{BeforeAndAfterEach, Suite}
 trait RunningTestEnvironment extends BeforeAndAfterEach {
   self: Suite =>
 
-  var env: TestEnvironment
+  var env = new TestEnvironment()
 
   override protected def beforeEach() = {
     env = new TestEnvironment()
