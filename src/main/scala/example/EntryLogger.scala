c4ad2ac8155ddb260d02daf33f52e97f9050042f
David Denton <denton.david@gmail.com>
2016-03-19 23:00:32 +0000

    dave - tweak entryLogger

--- src/main/scala/example/EntryLogger.scala
+++ src/main/scala/example/EntryLogger.scala
@@ -2,6 +2,7 @@ package example
 
 import java.time.Clock
 
+import com.twitter.finagle.http.Status._
 import com.twitter.finagle.{Service, Http}
 import com.twitter.finagle.http.Method.{Get, Post}
 import com.twitter.finagle.http.{Request, Response, Status}
@@ -24,7 +25,6 @@ object EntryLogger {
   }
 
   object LogList {
-    val entries = Body(bodySpec[Seq[UserEntry]]())
     val route = RouteSpec().at(Get) / "list"
   }
 }
@@ -34,7 +34,7 @@ object EntryLogger {
   */
 class EntryLogger(client: Service[Request, Response], clock: Clock) {
 
-  private def expect[T](expectedStatus: Status, b: Body[T]): Response => Future[T] = {
+  private def expectStatusAndExtract[T](expectedStatus: Status, b: Body[T]): Response => Future[T] = {
     r => if (r.status == expectedStatus) Future.value(b <-- r)
     else Future.exception(RemoteSystemProblem("entry logger", r.status))
   }
@@ -43,15 +43,15 @@ class EntryLogger(client: Service[Request, Response], clock: Clock) {
 
   def enter(username: Username): Future[UserEntry] =
     entryClient(Entry.entry --> UserEntry(username.value, goingIn = true, clock.instant().toEpochMilli))
-      .flatMap(expect(Status.Created, Entry.entry))
+      .flatMap(expectStatusAndExtract(Created, Entry.entry))
 
   private val exitClient = Exit.route bindToClient client
 
   def exit(username: Username): Future[UserEntry] =
     exitClient(Exit.entry --> UserEntry(username.value, goingIn = false, clock.instant().toEpochMilli))
-      .flatMap(expect(Status.Created, Exit.entry))
+      .flatMap(expectStatusAndExtract(Created, Exit.entry))
 
   private val listClient = LogList.route bindToClient client
 
-  def list(): Future[Seq[UserEntry]] = listClient().flatMap(expect(Status.Ok, LogList.entries))
+  def list(): Future[Seq[UserEntry]] = listClient().flatMap(expectStatusAndExtract(Ok, Body(bodySpec[Seq[UserEntry]]())))
 }
