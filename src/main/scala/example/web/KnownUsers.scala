ebee2d889a94273e80cd90f8ae13e91fa317ac52
David Denton <denton.david@gmail.com>
2017-01-10 10:14:37 +0000

    consistency

--- src/main/scala/example/web/KnownUsers.scala
+++
@@ -1,6 +0,0 @@
-package example.web
-
-import example.User
-import io.fintrospect.templating.View
-
-case class KnownUsers(users: Seq[User]) extends View
--- src/main/scala/example/web/ShowKnownUsers.scala
+++ src/main/scala/example/web/ShowKnownUsers.scala
@@ -3,9 +3,12 @@ package example.web
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.Method.Get
 import com.twitter.finagle.http.Request
+import example.User
 import example.external.UserDirectory
 import io.fintrospect.templating.View
 import io.fintrospect.{RouteSpec, ServerRoute}
+
+case class KnownUsers(users: Seq[User]) extends View
 
 object ShowKnownUsers {
   def route(userDirectory: UserDirectory): ServerRoute[Request, View] = {
